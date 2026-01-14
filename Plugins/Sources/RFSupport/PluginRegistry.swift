97577208bd310e13d6bb2516ace68692ee56a995
Mark Douma <mark@markdouma.com>
2026-01-13 19:33:40 -0500

    Also use ƒ for 'POST' resources.

--- Plugins/Sources/RFSupport/PluginRegistry.swift
+++ Plugins/Sources/RFSupport/PluginRegistry.swift
@@ -130,7 +130,7 @@ public class PluginRegistry {
             "exclamationmark.circle"
         case "finf":
             "f.cursive.circle"
-        case "FOND", "NFNT", "FONT", "sfnt":
+        case "FOND", "NFNT", "FONT", "sfnt", "POST":
             "f.cursive"
         case "GIFf", "jpeg", "PICT", "PNG ", "PNGf", "pxm#":
             "photo"
