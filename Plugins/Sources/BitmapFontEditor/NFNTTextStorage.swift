6f0cd86a3b91ee9be899bfc525e108aa233af9f6
Mark Douma <mark@markdouma.com>
2026-01-18 15:20:14 -0500

    Fixed typos.

--- Plugins/Sources/BitmapFontEditor/NFNTTextStorage.swift
+++ Plugins/Sources/BitmapFontEditor/NFNTTextStorage.swift
@@ -10,17 +10,17 @@ import RFSupport
 
 /// This typesetting architecture is loosely modeled after Cocoa's older
 /// style. At the top is `NFNTTextStorage`, which is a model for the string,
-/// and owns all the other objects else. `NFNTLayoutManager` uses `NFNTTextContainer`
+/// and owns all the other objects. `NFNTLayoutManager` uses `NFNTTextContainer`
 /// to model the area in which to set the type. It uses `NFNTTypesetter` to
 /// generate an array of `NFNTLineFragment`s, which are themselves an array
-/// of `NFNT,Glyph`s. The layout manager than does the drawing.///
+/// of `NFNT,Glyph`s. The layout manager then does the drawing.
 ///
 ///     `NFNTTextStorage`
 ///                |
 ///             `NFNTLayoutManager`
 ///               |      |       |
 /// `NFNTTextContainer`  |   `NFNTTypesetter`
-///                      |
+///                      |       |
 ///              `[NFNTLineFragment]`
 ///                      |
 ///                `[NFNT.Glyph]`
