94450b7e90423d87c2cac4a80106c9afc4bdc90c
Mark Douma <mark@markdouma.com>
2026-01-23 19:22:00 -0500

    Moved ResourceFileFormats to Formats subfolder in RFSupport. Removed original Formats folder & files.

--- ResForge/Formats/AppleDoubleFormat.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import RFSupport
-
-class AppleDoubleFormat: AppleSingleFormat {
-    override var name: String { NSLocalizedString("AppleDouble Archive", comment: "") }
-
-    override class var signature: UInt32 { 0x00051607 }
-}
