d8f0f88ef80346dc59788e8250eb4048271850b6
Mark Douma <mark@markdouma.com>
2026-04-17 15:04:00 -0400

    [CoreFont] BUGFIX for the last bugfix for creating contour's bezierPath:
    The entire iStart var is a remnant of Adobe's code and isn't really needed when the points are already split up into separate Contours. It's always 0.

--- Plugins/Sources/CoreFont/OTFFontFile/FontTables/FontTable_glyf/Contour.swift
+++ Plugins/Sources/CoreFont/OTFFontFile/FontTables/FontTable_glyf/Contour.swift
@@ -52,7 +52,6 @@ extension FontTable_glyf {
 
         public static func bezierPathRepresentation(of contours: [Contour], glyph: SimpleGlyph) -> NSBezierPath? {
             /* from /afdko/c/spot/source/glyf.c: */
-
             /* Draw path. This algorithm uses the point before (pt0) and the current point
                (pt1) in order to determine what PostScript path operators to emit. Any point
                can be on or off the curve giving 4 cases:
@@ -70,18 +69,17 @@ extension FontTable_glyf {
             var pt1: Point  /// current point
             var pt2: Point  /// next point
             var workingPoint: NSPoint = .zero
-            var iStart = 0 // start coordinate index
             let bPathRep = NSBezierPath()
             for contour in contours {
                 assert(contour.numPoints > 0)
                 let iEnd = contour.points.count - 1
                 pt0 = contour.points.last!
                 pt1 = contour.points.first!
-                if iStart == iEnd {
+                if iEnd == 0 {
                     // single point and/or empty contour
                 } else {
                     var nSegs = contour.points.count
-                    var i2 = iStart + 1 /// next coordinate index
+                    var i2 = 1 /// next coordinate index
                     pt2 = contour[i2]
                     if pt1.isOnCurve {
                         bPathRep.move(to: pt1.point)
@@ -90,7 +88,7 @@ extension FontTable_glyf {
                             /// back up to previous point
                             pt2 = pt1
                             pt1 = pt0
-                            i2 = iStart
+                            i2 = 0
                             bPathRep.move(to: pt1.point)
                         } else {
                             bPathRep.move(to: NSMakePoint((pt0.x + pt1.x) / 2.0,
@@ -106,28 +104,28 @@ extension FontTable_glyf {
                         /// get next point
                         i2 += 1
                         if i2 > iEnd {
-                            i2 = iStart /// wrap around
+                            i2 = 0 /// wrap around
                         }
                         pt2 = contour[i2]
                         if pt0.isOnCurve {
                             if pt1.isOnCurve {
-                                /// on on
+                                /// `on on`
                                 /// draw line
                                 bPathRep.line(to: pt1.point)
                             } else {
-                                /// on off
+                                /// `on off`
                                 /// begin bezier curve
                                 workingPoint = NSMakePoint((pt0.x + 2 * pt1.x) / 3.0,
                                                            (pt0.y + 2 * pt1.y) / 3.0)
                             }
                         } else {
                             if pt1.isOnCurve {
-                                /// off on
+                                /// `off on`
                                 /// end bezier path
                                 bPathRep.curve(to: pt1.point, controlPoint1: workingPoint, controlPoint2: NSMakePoint((2 * pt0.x + pt1.x) / 3.0,
                                                                                                                       (2 * pt0.y + pt1.y) / 3.0))
                             } else {
-                                /// off off
+                                /// `off off`
                                 /// end one bezier curve and begin another
                                 let x = (pt0.x + pt1.x) / 2.0
                                 let y = (pt0.y + pt1.y) / 2.0
@@ -142,7 +140,6 @@ extension FontTable_glyf {
                         nSegs -= 1
                     }
                 }
-                iStart = iEnd + 1
             }
             let hMetric = glyph.horizontalMetric
             let vMetric = glyph.verticalMetric
